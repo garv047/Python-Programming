bc38b60e3c1bf53573a9da00152290728cb27485
Garv Bhatia <bhatiagarv07@gmail.com>
2026-05-23 17:43:59 +0530

    error handling for division

---
+++ EXPERIMENT_7/4.py
@@ -0,0 +1,15 @@
+# 4.
+n = int(input())
+
+for _ in range(n):
+    try:
+        line = input().split()
+        a = int(line[0])
+        b = int(line[1])
+        print(a // b)
+    except ZeroDivisionError as e:
+        print(f"Error Code: {e}")
+    except ValueError as e:
+        print(f"Error Code: {e}")
+    except IndexError:
+        pass
