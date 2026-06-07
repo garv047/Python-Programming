e3c0e25f958d105eb46be0e7c88b2900217ee84b
Garv Bhatia <bhatiagarv07@gmail.com>
2026-06-07 13:03:25 +0530

    set operations

---
+++ EXPERIMENT_4/8.py
@@ -0,0 +1,9 @@
+# 8- set operations
+s1 = {"Red", "yellow", "orange", "blue"}
+s2 = {"violet", "blue", "purple"}
+
+print("Union:", s1 | s2)
+print("Intersection:", s1 & s2)
+print("Difference s1-s2:", s1 - s2)
+print("Difference s2-s1:", s2 - s1)
+print("Symmetric difference:", s1 ^ s2)
